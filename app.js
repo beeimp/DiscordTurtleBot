2b54798cd2bd9a2c492197b78eed8682be3b1a40
kogus <zero9231@naver.com>
2020-12-05 18:02:51 +0900

    connect db

--- app.js
+++ app.js
@@ -3,30 +3,24 @@ const db = require('./db_config');
 require('dotenv').config();
 const client = new Discord.Client();
 
-client.on('ready', ()=>{
+client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 })
-
-client.on('message', msg=>{
-    const helloArray = [
-        {word: 'hi', answer: "Hi~"},
-        {word: 'Hi', answer: "Hi~"},
-        {word: 'Hello', answer: "Hello~"},
-        {word: 'hello', answer: "Hello~"},
-        {word: '야', answer: "왜"},
-        {word: '거북아', answer: "왜"},
-        {word: "안녕", answer: "안녕!ㅎㅎ"},
-        {word: "ㅎㅇ", answer: "ㅎㅇ"}
-    ];
-    const result = helloArray.find(item => item.word === msg.content);
-
+client.on('message', msg => {
     // db에 메시지 저장
     db.InsertChatting([msg.author.username, msg.content, new Date(msg.createdTimestamp)]);
 
-    if (result && result.answer){
-        msg.reply(result.answer);
+    if (msg.content === '!전체삭제') {
+        for(let i = 0 ; i < 10; i++){
+            msg.channel.bulkDelete(100);
+        }
+    } else if (msg.content.startsWith('!추가')){
+        db.InsertAnswer(msg);
+    } else if (msg.content.startsWith('!수정')){
+        db.UpdateAnswer(msg);
+    } else{
+        db.SearchAnswer(msg);
     }
-
 });
 
 
--- db_config.js
+++ db_config.js
@@ -20,12 +20,77 @@ connection.connect((err) => {
 function createTableChatting() {
     connection.query('CREATE TABLE `chatting`(`c_id` int AUTO_INCREMENT PRIMARY KEY, `author` CHAR(20), `message` TEXT, `created_at` DATETIME)')
 }
+
+function createTableAnswer() {
+    connection.query('CREATE TABLE `answers`(`answers_id` int AUTO_INCREMENT PRIMARY KEY, `author` CHAR(20), `word` CHAR(200), `answer` CHAR(200), `created_at` DATETIME)')
+}
+
 function insertChatting(values) {
     connection.query('INSERT INTO chatting (author, message, created_at) VALUES (?, ?, ?)', values, (err, results, fields) => {
         if (err) throw err;
     })
 }
 
-module.exports.CreateTableChatting = createTableChatting;
+function searchAnswer(msg) {
+    connection.query('SELECT answer FROM answers WHERE word = ?', msg.content, (err, result, fields) => {
+        if (err) throw err;
+        if (result.length > 0 && result[0].answer) {
+            const answer = result[0].answer;
+            msg.reply(answer);
+        }
+    })
+}
 
+function insertAnswer(msg) {
+    const content = msg.content.split(' ');
+    if (content.length === 3) {
+        connection.query('SELECT * from answers WHERE word = ?', [content[1]], (err, result, fields) => {
+            if (err) throw err;
+            if (result && result.length) {
+                msg.reply('이미 등록된 질문입니다!');
+            } else {
+                connection.query(
+                    'INSERT INTO answers (author, word, answer, created_at) VALUES (?, ?, ?, ?)',
+                    [msg.author.username, content[1], content[2], new Date(msg.createdTimestamp)],
+                    (err, results, fields) => {
+                        if (err) {
+                            throw err;
+                        } else {
+                            msg.reply(`하나 배웠습니다!. ( 질문 : ${content[1]} / 답변 : ${content[2]} )`)
+                        }
+                    })
+            }
+        })
+    } else {
+        msg.reply('양식이 틀렸어요! (!추가 <질문> <답변>)');
+    }
+}
+function updateAnswer(msg) {
+    const content = msg.content.split(' ');
+    if (content.length === 3) {
+        connection.query('SELECT * from answers WHERE word = ?', [content[1]], (err, result, fields) => {
+            if (err) throw err;
+            if (result && result.length) {
+                connection.query(
+                    'UPDATE answers SET author = ?, word = ?, answer = ?, created_at = ? WHERE word = ?',
+                    [msg.author.username, content[1], content[2], new Date(msg.createdTimestamp), content[1]],
+                    (err, results, fields) => {
+                        if (err) throw err;
+                        else{
+                            msg.reply(`수정했습니다. ( 질문 : ${content[1]} / 답변 : ${content[2]} )`)
+                        }
+                    })
+            } else {
+            }
+        })
+    } else {
+        msg.reply('양식이 틀렸어요! (!수정 <질문> <답변>)');
+    }
+}
+
+module.exports.CreateTableChatting = createTableChatting;
+module.exports.CreateTableAnswer = createTableAnswer;
+module.exports.SearchAnswer = searchAnswer;
 module.exports.InsertChatting = insertChatting;
+module.exports.InsertAnswer = insertAnswer;
+module.exports.UpdateAnswer = updateAnswer;
