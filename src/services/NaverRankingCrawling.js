638551b2fbaa2111c72dd45e01c351d89a060251
kogus <zero9231@naver.com>
2021-08-24 22:44:54 +0900

    네이버 실검 삭제

---
+++ src/services/NaverRankingCrawling.js
@@ -0,0 +1,31 @@
+// 네이버 실검 삭제됨..ㅠㅠ
+// const  axios = require( 'axios');
+// const cheerio = require('cheerio');
+
+// const getHTML = async() => {
+//     try{
+//         return await axios.get('https://datalab.naver.com/keyword/realtimeList.naver');
+//     }catch(error){
+//         console.error(error);
+//     }
+// }
+
+// const webCrawling = (count = 20) => {
+//     return getHTML()
+//     .then( html => {
+//         let titleList = [];
+//         const $ = cheerio.load(html.data);
+//         let bodyList = $("#content > div > div.selection_area > div.selection_content > div.field_list > div > div > ul >  li.ranking_item > div> span.item_title_wrap > span.item_title");
+//         bodyList = count < 20 ? bodyList.slice(count) : bodyList;
+//         bodyList.each( (i, element) => {
+//             titleList.push({
+//                 rank: i+1,
+//                 title: $(element).text()
+//             })
+//         })
+
+//         return titleList;
+//     });
+// }
+
+// module.exports = webCrawling;
